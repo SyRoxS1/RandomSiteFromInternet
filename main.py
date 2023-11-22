a123074ca727f74e32155bdfea543434021042f8
SyRoxs <leo.moreaumenanteau@gmail.com>
2023-11-22 16:52:26 +0100

    update

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import subprocess
 import socket 
 import sys
 import requests
+import random
 
 def ping(ip): #response = 1 means host respond to ping
     res = subprocess.call(['ping', '-n', '1', ip], stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
@@ -39,28 +40,38 @@ def requestHTTP(ip):
     except:
         return False
     if r.status_code >= 200 and r.status_code <= 299:
-        return True
+        return r.status_code
     else:
         return False
+def checkResponseCode(code):
+    if code >= 100 and code <= 199:
+        return "informational"
+    if code >= 200 and code <= 299:
+        return "successful"
+    if code >= 300 and code <= 399:
+        return "redirection"
+    if code >= 400 and code <= 499:
+        return "client error"
+    if code >= 500 and code <= 599:
+        return "server error"
     
-ip_test = '192.58.214.244'
+    
+def randomIP():
+    a = random.randint(0,255)
+    b = random.randint(0,255)
+    c = random.randint(0,255)
+    d = random.randint(0,255)
+    return(str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d))
 
 
-if ping(ip_test) == 0: 
-    hostup = True
-    print("OK") 
-
-else:
-    hostup = False
-    print("NO")
 
 
 
 
 
 for i in range(255):
-    ip = "172.217.20." + str(i)
-    print("scanning ip : " + ip)
+    ip = randomIP()
+    
     if portscan(ip,"80") == 0:
         print("IP : "+ ip + " is up doing http request ...")
         requestHTTP(ip)
@@ -69,8 +80,7 @@ for i in range(255):
         else:
             print("not up")
 
-    else:
-        print("IP : "+ ip + " is down")
+
 
 
 
