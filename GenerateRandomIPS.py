d85761b7f719de8bbdf318a0ff133684d3f29fa5
SyRoxS1 <114361806+SyRoxS1@users.noreply.github.com>
2023-11-27 10:04:33 +0100

    Update GenerateRandomIPS.py

--- GenerateRandomIPS.py
+++ GenerateRandomIPS.py
@@ -10,7 +10,7 @@ def randomIP():
                 (a == 10) or
                 (a == 172 and 16 <= b <= 31) or
                 (a == 192 and b == 168) or
-                (a == 127) #i dont know why but most 127 who respond are down
+                (a == 127) #dont allow loopback addresses too
             ):
             return(str(a)+'.'+str(b)+'.'+str(c)+'.'+str(d))
     
